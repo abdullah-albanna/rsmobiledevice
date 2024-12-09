32a0507ac621fb6e270f1b41194eb705ae60bdff
Abdullah-Albanna <albannaa78@gmail.com>
2024-12-09 23:24:53 +0300

    device_diagnostic: added _get_diagnostic_relays for DeviceGroup

--- src/device_diagnostic/mod.rs
+++ src/device_diagnostic/mod.rs
@@ -1,8 +1,13 @@
-use crate::{device::DeviceClient, devices_collection::SingleDevice};
+use crate::{
+    device::DeviceClient,
+    devices_collection::{DeviceGroup, SingleDevice},
+};
 use enums::{DevicePowerAction, DiagnosticBehavior, DiagnosticType, IORegPlane};
 use errors::DeviceDiagnosticError;
 use plist_plus::Plist;
-use rusty_libimobiledevice::services::diagnostics_relay::DiagnosticsRelay;
+use rusty_libimobiledevice::services::{
+    diagnostics_relay::DiagnosticsRelay, lockdownd::LockdowndService,
+};
 use std::marker::PhantomData;
 
 pub mod enums;
@@ -56,7 +61,6 @@ impl DeviceDiagnostic<SingleDevice> {
     }
 
     pub fn query_ioreg_plane(&self, plane: IORegPlane) -> Result<Plist, DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         let relay = self._get_diagnostic_relay()?;
         Ok(relay.query_ioregistry_plane(plane.to_string())?)
     }
@@ -65,7 +69,6 @@ impl DeviceDiagnostic<SingleDevice> {
         &self,
         key: impl Into<String>,
     ) -> Result<Plist, DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         let relay = self._get_diagnostic_relay()?;
         Ok(relay.query_ioregistry_entry(key, "")?)
     }
@@ -74,7 +77,6 @@ impl DeviceDiagnostic<SingleDevice> {
         &self,
         keys: Vec<impl Into<String>>,
     ) -> Result<Plist, DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         let relay = self._get_diagnostic_relay()?;
         let mut plist = Plist::new_array();
 
@@ -88,20 +90,47 @@ impl DeviceDiagnostic<SingleDevice> {
         &self,
         r#type: DiagnosticType,
     ) -> Result<Plist, DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         let relay = self._get_diagnostic_relay()?;
         Ok(relay.request_diagnostics(r#type.to_string())?)
     }
     pub fn sleep(&self) -> Result<(), DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         self._device_power_action(DevicePowerAction::Sleep)
     }
     pub fn restart(&self, flag: DiagnosticBehavior) -> Result<(), DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         self._device_power_action(DevicePowerAction::Restart(flag))
     }
     pub fn shutdown(&self, flag: DiagnosticBehavior) -> Result<(), DeviceDiagnosticError> {
-        self.device.check_connected::<DeviceDiagnosticError>()?;
         self._device_power_action(DevicePowerAction::Shutdown(flag))
+    }
+}
+
+impl DeviceDiagnostic<DeviceGroup> {
+    pub fn _get_diagnostic_relays(&self) -> Result<Vec<DiagnosticsRelay>, DeviceDiagnosticError> {
+        let devices = self.device.get_devices();
+
+        let mut lockdownds = self
+            .device
+            .get_lockdownd_clients::<DeviceDiagnosticError>()?;
+
+        let diagnostic_services: Vec<LockdowndService> = lockdownds
+            .iter_mut()
+            .map(|lockdownd| {
+                lockdownd
+                    .start_service(DIAGNOSTICS_RELAY_SERVICE, true)
+                    .map_err(|err| DeviceDiagnosticError::ServiceError(err.to_string()))
+            })
+            .collect::<Result<Vec<_>, DeviceDiagnosticError>>()?;
+
+        // Combine diagnostic services and devices, handling any errors during the mapping
+        let relays: Result<Vec<DiagnosticsRelay>, DeviceDiagnosticError> = diagnostic_services
+            .into_iter()
+            .zip(devices.iter())
+            .map(|(service, device)| {
+                DiagnosticsRelay::new(device, service)
+                    .map_err(|err| DeviceDiagnosticError::RelayInitializationError(err.to_string()))
+            })
+            .collect();
+
+        relays
     }
 }
